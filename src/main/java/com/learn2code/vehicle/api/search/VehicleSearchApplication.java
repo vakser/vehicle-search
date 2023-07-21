307f43a2bafc05d8f388f8a8bb8fb7fd3c986475
vakser <65089446+vakser@users.noreply.github.com>
2023-07-21 09:46:02 +0300

    project initial setup

---
+++ src/main/java/com/learn2code/vehicle/api/search/VehicleSearchApplication.java
@@ -0,0 +1,13 @@
+package com.learn2code.vehicle.api.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleSearchApplication.class, args);
+	}
+
+}
