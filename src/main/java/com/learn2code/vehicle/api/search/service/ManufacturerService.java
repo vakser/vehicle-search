3187cbe5b1a96441f162f24847e1ebab92ce4031
vakser <65089446+vakser@users.noreply.github.com>
2023-07-26 20:29:41 +0300

    added rest endpoint to get all manufacturers

--- src/main/java/com/learn2code/vehicle/api/search/service/ManufacturerService.java
+++ src/main/java/com/learn2code/vehicle/api/search/service/ManufacturerService.java
@@ -2,6 +2,10 @@ package com.learn2code.vehicle.api.search.service;
 
 import com.learn2code.vehicle.api.search.entity.Manufacturer;
 
+import java.util.List;
+
 public interface ManufacturerService {
     Manufacturer saveManufacturer(Manufacturer manufacturer);
+
+    List<Manufacturer> fetchAllManufacturers();
 }
