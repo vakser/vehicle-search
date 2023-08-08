4236db79064a4a46e6c4d5cb36873e2a4e2dead3
vakser <65089446+vakser@users.noreply.github.com>
2023-08-08 13:24:19 +0300

    Error handler created for No Manufacturer Found

---
+++ src/main/java/com/learn2code/vehicle/api/search/exception/ManufacturerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.learn2code.vehicle.api.search.exception;
+
+public class ManufacturerNotFoundException extends Exception {
+    public ManufacturerNotFoundException(String message) {
+        super(message);
+    }
+}
